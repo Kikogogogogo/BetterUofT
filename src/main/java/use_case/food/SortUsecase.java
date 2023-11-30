c6744c55f9f8ce8bcc967177f767c922c4cd21d6
eric-qli <92590396+eric-qli@users.noreply.github.com>
2023-11-29 22:59:03 -0500

    finished sorting

---
+++ src/main/java/use_case/food/SortUsecase.java
@@ -0,0 +1,25 @@
+package use_case.food;
+
+import Data.CsvFoodRepo;
+import Entity.Food;
+
+import java.util.ArrayList;
+
+import View.food.FoodApp;
+import use_case.food.QuickSort;
+
+public class SortUsecase {
+    private static ArrayList<Food> foodItems = new ArrayList<>();
+
+    public void sortFood() {
+        CsvFoodRepo csvFoodRepo = new CsvFoodRepo("food.csv");
+        foodItems = csvFoodRepo.getAllFoods();
+        QuickSort quickSort = new QuickSort(foodItems, 0, foodItems.size() - 1);
+        foodItems = quickSort.run();
+        CsvFoodRepo.emptyFoodFile();
+        CsvFoodRepo.saveAllFoods(foodItems);
+        FoodApp foodApp = new FoodApp();
+        foodApp.setVisible(true);
+    }
+
+}
