f9be87e98366e0496e85aa26e1185e642ae4d9b4
aj3616 <amyzh.jin@mail.utoronto.ca>
2023-12-05 00:45:39 -0500

    resolve conflict

--- src/main/java/Entity/TradeItem.java
+++ src/main/java/Entity/TradeItem.java
@@ -1,35 +1,22 @@
 package Entity;
 
-import java.sql.Timestamp;
-
-public abstract class TradeItem implements entity.Item {
-    private String name;
+public class TradeItem{
     private String description;
-    private Timestamp timestamp;
-    private String id;
     private boolean traded;
     private String category;
+    private double price;
+    private String id;
 
-    public TradeItem(String name, String description, String date, String time, Timestamp timestamp, String id, boolean status, String category) {
-        this.name = name;
+    public TradeItem(String description, boolean status, String category, double price) {
         this.description = description;
-        this.timestamp = timestamp;
-        this.id = id;
         this.traded = status;
         this.category = category;
-    }
-
-    public String getName() {
-        return name;
+        this.price = price;
     }
 
     public String getDescription() {
         return description;
     }
-
-    public Timestamp getTimestamp() { return timestamp; }
-
-    public String getId() { return id; }
 
     public boolean getStatus() { return traded; }
 
@@ -41,5 +28,33 @@ public abstract class TradeItem implements entity.Item {
 
     public String getCategory() {
         return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String toCsvString() {
+        return category + "," + traded + "," + description + "," + price;
+    }
+    public static TradeItem fromCsvString(String csvString) {
+        String[] data = csvString.split(",");
+        String category = data[0].trim();
+        boolean traded = Boolean.parseBoolean(data[1]);
+        String description = data[2].trim();
+        double price = Double.parseDouble(data[3].trim());
+
+        return new TradeItem(description, traded, category, price);
     }
 }
