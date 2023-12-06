0c811b7562dad6f96da76709eb80bebe3fe21fec
YizhuoLiu <101411735+YizhuoLiu@users.noreply.github.com>
2023-12-06 02:44:42 -0500

    Update Report.java

--- src/main/java/Entity/Report.java
+++ src/main/java/Entity/Report.java
@@ -6,9 +6,10 @@ public class Report {
     private Long reportId;
     private Long userId;
     private Long itemId;
-    private Timestamp timestamp;
+    private Timestamp timestamp ;
     private String description;
 
+    public Report() {}
 
     public Long getReportId() {
         return reportId;
@@ -16,6 +17,30 @@ public class Report {
 
     public void setReportId(Long reportId) {
         this.reportId = reportId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
     }
 
     public String getDescription() {
